36b9d6998e28cf93ae9985494e404423017e91ea
lethanhan01 <An.LT235631@sis.hust.edu.vn>
2025-08-16 22:25:35 +0700

    bo sung

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import "./TotalCost.css";
+import { configureStore } from '@reduxjs/toolkit';
+import venueReducer from './venueSlice.js';
+import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
-const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
-
-  return (
-    <div className="pricing-app">
-      <div className="display_box">
-        <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
-        </div>
-        <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
-         
-            <div>
-             
-            </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default TotalCost;
+export default configureStore({
+  reducer: {
+    venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
+  },
+});
